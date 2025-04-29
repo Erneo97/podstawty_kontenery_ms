6289285e84e50afde36b5e1ac75f5ff4c181be91
Erneo97 <michalkaniewski1997@gmail.com>
2025-04-29 18:09:22 +0200

    Stworzenie klas pomocniczych.
    Szablonowe (BaseModel)
    Enumy wykorzystywane później przez użytkowników

---
+++ backend/klasy/enumValue.py
@@ -0,0 +1,28 @@
+from enum import Enum
+
+class UserGoal(str, Enum):
+    """Enumerate Celów jakie użytkownicy chcą osiągnąć
+        dostępne 3 cele:
+        1) Redukacja masy ciała
+        2) Budowa masy mięścniowej
+        3) Utrzymanie wagi
+
+    Umożliwia porównanie enum ze stringiem orzymanym bezpośrednio z bazy
+        """
+    WEIGHT_REDUCE = "Redukcja" #redukacja masy ciała
+    MASS_BUILDING = "Budowa masy"
+    WEIGHT_CONST = "const"
+
+class UserGender(str, Enum):
+    """Wartość enumerate dostępnych płci dla uzytkowników. Posiada 3 wartości
+    mężczyzna, kobieta, ora inna.
+    Umożliwia porównanie enum ze stringiem orzymanym bezpośrednio z bazy
+    """
+    MALE = "M"
+    FEMALE = "F"
+    OTHER = "O"
+
+class MuscleGroup(str, Enum):
+    """Wartość enumerate dostępnych grupy mięśniowej"""
+
+    pass
---
+++ backend/klasy/userbases_to_comunicate.py
@@ -0,0 +1,44 @@
+from fastapi.openapi.models import Schema
+from pydantic import BaseModel, Field
+
+"""Wszystkie klasy oparte na BseModel, któe klient wymienia z serewerem"""
+
+
+class UserBase(BaseModel):
+    """Bazowa kalsa do serializacji danych o użytkowniku"""
+
+    imie: str = Field(..., max_length=50)
+    nazwisko:  str = Field(..., max_length=50)
+    email: str
+    haslo: str = Field(..., min_length=5)
+    plec: str | None = Field(None, max_length=10)
+    wzrost: float | None
+    cel: str | None = Field(None, max_length=100)
+    waga: float | None
+
+    def values(self):
+        return list(self.model_dump().values())
+
+class UserOut(BaseModel):
+    """Bazowa kalsa do serializacji publicznego id_użytkownika potwierdzajacego udaną operację"""
+    id_uzytkownika: str
+
+    def values(self):
+        return list(self.model_dump().values())
+
+class LoginData(Schema):
+    """Bazowa kalsa do serializacji danych logoawania"""
+    email: str
+    haslo: str
+
+    def values(self):
+        return list(self.model_dump().values())
+
+class UserWeight(BaseModel):
+    """Bazowa klasa do serializacji danych nowej wagi danego użytkownika"""
+    id: str
+    waga: float
+
+    def values(self):
+        return list(self.model_dump().values())
+
